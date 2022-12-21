18802d3ff2c4c14d27b35e26332536c1664f7366
zeroet <y36609672@gmail.com>
2022-12-22 00:01:25 +0100

    in

--- srcs/requirements/mariadb/tools/setup_db.sh
+++ srcs/requirements/mariadb/tools/setup_db.sh
@@ -2,13 +2,17 @@
 
 service mysql start
 
-#mysql migration utf8 -> utf8_unicode_ci option DEFAULT CHARACTER SET utf8(general) COLLATE utf8_unicode_ci
-echo "CREATE DATABASE IF NOT EXISTS $MARIADB_ID" | mysql -u root
-
+# DB create
+echo "CREATE DATABASE IF NOT EXISTS $MARIADB_ID DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci" | mysql -u root
+# create user
 echo "CREATE USER IF NOT EXISTS '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PW'" | mysql -u root
+# authentification : GRANT user autentification command
+echo "GRANT ALL PRIVILEGES ON $MARIADB_ID.* TO '$MARIADB_USER'@'%'" | mysql -u root
 
-echo "GRANT ALL ON $MARIADB_ID.* TO '$MARIADB_USER'@'%'" | mysql -u root
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PW'" | mysql -u root
+echo "FLUSH PRIVILEGES" | mysql -u root -p $MARIADB_ROOT_PW
 
 service mysql stop
 
-exec /usr/sbin/mysqld -u root
+# launch mysqsl daemon with mysqld (or mysqld_safe for security)
+exec mysqld -u root -p $MYRIADB_ROOT_PW
--- srcs/requirements/wordpress/tools/setup_wp.sh
+++ srcs/requirements/wordpress/tools/setup_wp.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
+PROTECTION="/var/www/html/wordpress/.protection"
 
+if  [ ! -f "$PROTECTION" ]; then
+	# Show wp info
+	wp --info
+	# Download wordpress core
+	wp core download --allow-root
+	# Create wp-config.php
+	wp config create --dbname=$MARIADB_ID --dbuser=$MARIADB_USER --dbpass=$MARIADB_PW --dbhost="mariadb" --path="/var/www/html/wordpress/" --allow-root --skip-check
+	# Create admin user
+	wp core install --url=$WORDPRESS_URL --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --path="/var/www/html/wordpress/" --allow-root
+	# Create normal user
+	wp user create $WORDPRESS_DFT_USER $WORDPRESS_DFT_EMAIL --role=author --user_pass=$WORDPRESS_DFT_PASSWORD --allow-root
+	touch $PROTECTION
+fi
+exec php-fpm7.3 --nodaemonize
 
+# see more
+# wp core: https://developer.wordpress.org/cli/commands/core/
+# wp config: https://developer.wordpress.org/cli/commands/config/create/
+# wp users: https://developer.wordpress.org/cli/commands/user/
